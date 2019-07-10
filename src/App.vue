26ffa176aa3a26b231e5581b1a0a965b539a9be8
Farshad Fakhar <farshadfakhar@Farshads-MacBook-Pro-2.local>
2019-07-10 20:22:26 +0430

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NumberKeyboard :show="true"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NumberKeyboard from './components/NumberKeyboard'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    NumberKeyboard
   }
 }
 </script>
